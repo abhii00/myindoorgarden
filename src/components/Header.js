ba28197c2f63ab169052d6c8d8f3fa77344cec67
abhi00 <abhijit.pandit808@gmail.com>
2021-07-08 20:24:52 +0100

    formatting changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 class Header extends React.Component{
     render(){
         return(
-            <React.Fragment>
+            <div className="header">
                 <div className="title">
                     <p>
                         My Indoor Garden
@@ -12,7 +12,7 @@ class Header extends React.Component{
                 <div className="headingline">
                     &nbsp;
                 </div>   
-            </React.Fragment>
+            </div>
         )
     }
 }
--- src/components/MyIndoorGarden.js
+++ src/components/MyIndoorGarden.js
@@ -20,7 +20,7 @@ class MyIndoorGarden extends React.Component{
 
     render(){
         var plants = Array(5).fill(
-            <Plant renderFactfile={this.renderFactfile.bind(this)} name="Kentia" pic="https://cdn.shopify.com/s/files/1/1706/1307/products/Howea-forsteriana-Kentia-Palm-Raw-Cody-Tall-Plant-Vase-Running-Rust_61b38fd3-218c-4669-9042-d4c6b040a36b_1600x.jpg?v=1617186033"/>
+            <Plant renderFactfile={this.renderFactfile.bind(this)} name="Kentia Palm" pic="https://cdn.shopify.com/s/files/1/1706/1307/products/Howea-forsteriana-Kentia-Palm-Raw-Cody-Tall-Plant-Vase-Running-Rust_61b38fd3-218c-4669-9042-d4c6b040a36b_1600x.jpg?v=1617186033"/>
         )
 
         return(
