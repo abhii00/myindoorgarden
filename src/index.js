6d93eec8099a5a546a52cf1cca65317581e78edd
abhi00 <abhijit.pandit808@gmail.com>
2021-07-08 02:12:33 +0100

    empty overall container

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './index.css'
+import Stand from './components/Plant.js'
+import Factfile from './components/Plant.js'
+import Plant from './components/Plant.js'
+
+ReactDOM.render(
+    <PendulumContainer />,
+    document.getElementById('root')
+);
