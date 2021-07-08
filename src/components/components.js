167ca4fb5080cf4d3e45cf26ba9bc6fe3d0712c0
abhi00 <abhijit.pandit808@gmail.com>
2021-07-08 02:20:31 +0100

    components.js file

---
+++ src/components/components.js
@@ -0,0 +1,13 @@
+import Plant from './Plant.js'
+import Factfile from './Factfile.js'
+import Stand from './Stand.js'
+import Header from './Header.js'
+import MyIndoorGarden from './MyIndoorGarden'
+
+export {
+    Plant,
+    Factfile,
+    Stand,
+    Header,
+    MyIndoorGarden
+}
--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Factfile, Plant, Stand, Header, MyIndoorGarden } from './components/components.js'
 
 import './index.css'
-import Factfile from './components/Factfile.js'
-import Plant from './components/Plant.js'
-import Stand from './components/Stand.js'
-import Header from './components/Header.js'
-import MyIndoorGarden from './components/MyIndoorGarden'
 
 ReactDOM.render(
     <MyIndoorGarden />,
