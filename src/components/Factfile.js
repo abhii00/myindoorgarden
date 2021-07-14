9cc1c7607856fbf3cc77e544a9e846480aea3e9b
abhi00 <abhijit.pandit808@gmail.com>
2021-07-14 18:08:13 +0100

    template for factfile

--- src/components/Factfile.js
+++ src/components/Factfile.js
@@ -4,6 +4,14 @@ class Factfile extends React.Component{
     render(){
         return(
             <div className="factfile">
+                <p>Nickname: {}</p>
+                <p>Species: {}</p>
+                <p>Date of Birth: {}</p>
+
+                <p>When I was Last...</p>
+
+                <p>Watered: {}</p>
+                <p>Fed: {}</p>
                 &nbsp;
                 <a className="cross" onClick={this.props.unrenderFactfile} href="/#"> X </a>
             </div>
