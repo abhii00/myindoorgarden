179c9aebb9f9a4cda991978ee962d1c9b344c715
abhi00 <abhijit.pandit808@gmail.com>
2021-07-15 23:51:54 +0100

    unfunctional icons added for mood and pot

--- src/components/Factfile.js
+++ src/components/Factfile.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import water from '../assets/water.svg'
 import food from '../assets/food.svg'
+import mood from '../assets/mood.svg'
+import pot from '../assets/pot.svg'
 
 class Factfile extends React.Component{
     constructor(props){
@@ -18,21 +20,27 @@ class Factfile extends React.Component{
     render(){
         return(
             <div className="factfile-main">
-                <p className="factfile-nickname">{this.state.plant.nickname}</p>
-                <p className="factfile-species">{this.state.plant.species}</p>
+                <div className="factfile-nickname">{this.state.plant.nickname}</div>
+                <div className="factfile-species">{this.state.plant.species}</div>
+
+                <div className="factfile-icons">
+                    <picture className="factfile-icons-mood">
+                        <img className="factfile-icons-mood-image" src={mood} alt=""></img>
+                    </picture>
+                    <picture className="factfile-icons-watering">
+                        <img className="factfile-icons-watering-image" src={water} alt=""></img>
+                    </picture>
+                    <picture className="factfile-icons-feeding">
+                        <img className="factfile-icons-feeding-image" src={food} alt=""></img>
+                    </picture>
+                    <picture className="factfile-icons-pot">
+                        <img className="factfile-icons-pot-image" src={pot} alt=""></img>
+                    </picture>
+                </div>
 
                 <picture className="factfile-pic">
                     <img className="factfile-pic-image" src={this.state.plant.url} alt=""></img>
                 </picture>
-
-                <div className="factfile-icons">
-                    <picture className="factfile-icons-watering">
-                        <img src={water} alt=""></img>
-                    </picture>
-                    <picture className="factfile-icons-feeding">
-                        <img src={food} alt=""></img>
-                    </picture>
-                </div>
                 
                 <a className="factfile-cross" onClick={this.props.unrenderFactfile} href="/#">X</a>
             </div>
