93e42f8cc7784777bf009c74237b995779678689
abhi00 <abhijit.pandit808@gmail.com>
2021-07-16 17:14:05 +0100

    handles icon data of varying digits

--- src/components/Icon.js
+++ src/components/Icon.js
@@ -2,10 +2,24 @@ import React from 'react'
 
 class Icon extends React.Component{
     render(){
+        var font_size;
+        var top;
+        if (this.props.data < 10){
+            font_size = "2vw"
+            top = "10%"
+        }
+        else if (this.props.data > 10 && this.props.data < 100){
+            font_size = "1.5vw";
+            top = "25%"
+        }
+        else if (this.props.data > 100){
+            font_size = "1vw";
+            top = "40%"
+        }
         return(
             <div className="icon">
                 <img style={{backgroundColor: this.props.color}} className="icon-image" src={this.props.image} alt=""/>
-                <div className="icon-data">{this.props.data}</div>
+                <div style={{fontSize: font_size, top: top}} className="icon-data">{this.props.data}</div>
             </div>
         )
     }
